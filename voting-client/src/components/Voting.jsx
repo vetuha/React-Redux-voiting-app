97ba2b9257ddef58c67af8236b7172778778977f
vetuha <dev.vitaliimoroz@gmail.com>
2017-03-07 23:04:11 +0200

    added couple of ui tests, started working on UI with React

---
+++ voting-client/src/components/Voting.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default React.createClass({
+  getPair: function() {
+    return this.props.pair || [];
+  },
+  render: function() {
+    return <div className="voting">
+      {this.getPair().map(entry =>
+        <button key={entry}
+                onClick={() => this.props.vote(entry)}>
+          <h1>{entry}</h1>
+        </button>
+      )}
+    </div>;
+  }
+});
